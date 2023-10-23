54390ae32450e102f90d2cdfd514495fe787b75d
Loïc Dörr <loic.debeuf@gmail.com>
2023-10-23 09:34:13 +0200

    initial commit

---
+++ backend/src/database/userEntityService.ts
@@ -0,0 +1,22 @@
+import { User } from '../types'
+
+export class DatabaseServcice {
+  async getAll(): Promise<User[]> {
+  }
+
+  async getOneById(id: string): Promise<User> {
+
+  }
+
+  async insert(): Promise<void> {
+
+  }
+
+  async update(): Promise<void> {
+
+  }
+
+  async delete(id: string): Promise<void> {
+
+  }
+}
