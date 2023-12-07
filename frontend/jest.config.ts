db2c64e5386d6e7327e04df6d1e55c14bbe6bd40
Alexander Quadt <alexander.quadt@haufe-akademie.de>
2023-12-07 13:54:56 +0100

    fix test framework

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,7 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest',
   transform: { '^.+\\.ts?$': 'ts-jest' },
+  testEnvironment: 'jsdom',
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
-}
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)']
+};
