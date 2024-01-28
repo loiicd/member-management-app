bbea30cc90c7309615ec58290bc56b319455bf21
Loïc Dörr <loic.debeuf@gmail.com>
2024-01-28 17:36:54 +0100

    change user tabel

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -13,6 +13,7 @@ import Button from '../components/base/button'
 import Input from '../components/base/input'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import UserTable from '../components/userTable'
 
 const DashboardPage = () => {
   const navigate = useNavigate()
@@ -24,10 +25,10 @@ const DashboardPage = () => {
       .then((data) => setUsers(data)) 
   }, [searchTerm])
 
-  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = e.target.value.toLowerCase()
-    setSearchTerm(searchTerm)
-  }
+  // const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const searchTerm = e.target.value.toLowerCase()
+  //   setSearchTerm(searchTerm)
+  // }
 
   return (
     <main>
@@ -35,36 +36,14 @@ const DashboardPage = () => {
       <div className='container mx-auto'>
         <div className='flex justify-between pb-2'>
           <Typography variant='h3'>User</Typography>
-          <div className='flex space-x-2'>
+          {/* <div className='flex space-x-2'>
             <Input type='text' placeholder='Suche ...' onChange={handleSearch}/>
             <Button variant='outlined'>Export</Button>
             <UserDialog type='insert' />
-          </div>
+          </div> */}
         </div>
-        <Table>
-          <TableHead>
-            <tr>
-              <TableCell>Vorname</TableCell>
-              <TableCell>Nachname</TableCell>
-              <TableCell>Geburtsdatum</TableCell>
-              <TableCell>Adresse</TableCell>
-              <TableCell>Einsatzqualifikation</TableCell>
-              <TableCell>Online Zugang</TableCell>
-            </tr>
-          </TableHead>
-          <TableBody>
-            {users.map((user) => (
-              <tr key={user.id} className='bg-white border-b dark:bg-zinc-800 dark:border-zinc-700 hover:bg-zinc-200 dark:hover:bg-zinc-600' onClick={() => navigate(`/44484414-a4db-4717-8507-26f5296409dd/user/${user.id}`)}> {/* HardCoded URL ACCOUNT */}
-                <TableCell>{user.firstname}</TableCell>
-                <TableCell>{user.lastname}</TableCell>
-                <TableCell>{user.birthdate?.toLocaleDateString('de', { day: '2-digit', month: '2-digit', year: 'numeric' })}</TableCell>
-                <TableCell>{user.address}</TableCell>
-                <TableCell>{user.qualifications.map((qualification) => qualification.abbreviation + ' ')}</TableCell>
-                <TableCell>{user.webaccess ? <FontAwesomeIcon icon={icon({ name: 'check', style: 'solid' })} size='xl' className='text-lime-600' /> : <FontAwesomeIcon icon={icon({ name: 'xmark', style: 'solid' })} size='xl' className='text-red-600' />}</TableCell>
-              </tr> 
-            ))}
-          </TableBody>
-        </Table>
+
+        <UserTable users={users} />
       </div>
     </main>
   )
